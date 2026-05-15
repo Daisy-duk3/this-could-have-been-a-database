5aa4ea1116f9f54cc5c4e6b871a786492dada9d6
daisy <daisyjayne01@gmail.com>
2026-05-15 08:30:06 +0100

    Add transaction signing and validation with elliptic keys

--- blockchain_demo/main.js
+++ blockchain_demo/main.js
@@ -1,122 +1,29 @@
-import CryptoJS from 'crypto-js';
+import { Blockchain, Transaction } from './blockchain.js';
 
-// Testing package works
-//const hash = CryptoJS.SHA256("hello").toString();
-//
-//console.log(hash);
+const myChain = new Blockchain();
 
-class Transaction{
-    constructor(fromAddress, toAddress, amount){
-        this.fromAddress = fromAddress;
-        this.toAddress = toAddress;
-        this.amount = amount;
-    }
-}
+import elliptic from 'elliptic';
 
-class Block {
-    constructor(timestamp, transactions, previousHash =''){
-        this.timestamp = timestamp;
-        this.transactions = transactions;
-        this.previousHash = previousHash;
-        this.nonce = 0;
-        this.hash = this.calculateHash();
-    }
+const EC = elliptic.ec;
+const ec = new EC('secp256k1');
 
-    calculateHash(){
-    return CryptoJS.SHA256(this.previousHash +this.timestamp +JSON.stringify(this.transactions) + this.nonce).toString();
-    }
-
-    mineBlock(difficulty){
-        while(this.hash.substring(0, difficulty) !== Array(difficulty + 1). join("0")){
-            this.nonce++;
-            this.hash = this.calculateHash();
-        }
-
-        console.log("Block mined: " + this.hash);
-    }
-}
-
-
-
-class Blockchain{
-    constructor(){
-        this.chain = [this.createGenesisBlock()];
-        this.difficulty = 2;
-        this.pendingTransactions = [];
-        this.miningReward = 100;
-    }
-
-    createGenesisBlock(){
-        return new Block("08/05/2026", "Genesis block", "0");
-    }
-
-    getLatestBlock(){
-        return this.chain[this.chain.length -1];
-    }
-
-    minePendingTransactions(miningRewardAddress){
-        let block = new Block(Date.now(), this.pendingTransactions, this.getLatestBlock().hash);
-        block.mineBlock(this.difficulty);
-
-        console.log('Block successfully mined!');
-        this.chain.push(block);
-
-        this.pendingTransactions = [
-            new Transaction(null, miningRewardAddress, this.miningReward)
-        ];
-    }
-
-    createTransaction(transaction){
-        this.pendingTransactions.push(transaction);
-    }
-
-    getBalanceOfAddress(address){
-        let balance = 0;
-
-        for(const block of this.chain){
-            for(const trans of block.transactions){
-                if(trans.fromAddress === address){
-                    balance -= trans.amount;
-                }
-
-                if(trans.toAddress === address){
-                    balance += trans.amount;
-                }
-            }
-        }
-
-        return balance;
-    }
-
-    isChainValid(){
-        for(let i = 1; i < this.chain.length; i++){
-            const currentBlock = this.chain[i];
-            const previousBlock = this.chain[i - 1];
-
-            if(currentBlock.hash !== currentBlock.calculateHash()){
-                return false;
-            }
-
-            if(currentBlock.previousHash !== previousBlock.hash){
-                return false;
-            }
-        }
-
-        return true;
-    }
-}
+const myKey = ec.keyFromPrivate(''); // run key generator and make a note of both public and private keys put private here
+const myWalletAddress = myKey.getPublic('hex');
 
 let BubbaCoin = new Blockchain();
 
-BubbaCoin.createTransaction(new Transaction('address1', 'address2', 100));
-BubbaCoin.createTransaction(new Transaction('address2', 'address1', 50));
+const tx1 = new Transaction(myWalletAddress, 'public key goes here', 10);
+tx1.signTransaction(myKey);
+BubbaCoin.addTransaction(tx1);
 
 console.log('\nStarting the miner...');
-BubbaCoin.minePendingTransactions('Bubbas-address');
+BubbaCoin.minePendingTransactions(myWalletAddress);
 
-console.log('\nBalance of Bubba is: ', BubbaCoin.getBalanceOfAddress('Bubbas-address'));
+console.log('\nBalance BEFORE reward is mined: ', BubbaCoin.getBalanceOfAddress(myWalletAddress));
 
-console.log('\nStarting the miner again...');
-BubbaCoin.minePendingTransactions('Bubbas-address');
+console.log('\nMining reward block...');
+BubbaCoin.minePendingTransactions(myWalletAddress);
 
-console.log('\nBalance of Bubba is: ', BubbaCoin.getBalanceOfAddress('Bubbas-address'));
+console.log('\nBalance AFTER reward is mined: ', BubbaCoin.getBalanceOfAddress(myWalletAddress));
+
+console.log('Is chain valid?', BubbaCoin.isChainValid());
